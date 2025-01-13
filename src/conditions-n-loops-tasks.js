766d2d71352a10b3560011200706c0b94f8b7ff4
Nerzula <alexzloid@gmail.com>
2025-01-13 22:22:02 +0300

    feat: resolve shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -454,8 +454,25 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+    let arr = str.split('');
+  
+    for (let i = 0; i < iterations; i++) {
+      let even = '';
+      let odd = '';
+  
+      for (let j = 0; j < arr.length; j++) {
+        if (j % 2 === 0) {
+          even += arr[j];
+        } else {
+          odd += arr[j];
+        }
+      }
+  
+      arr = (even + odd).split('');
+    }
+  
+    return arr.join('');
 }
 
 /**
